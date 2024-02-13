53ce9b3252949548156866f174959bc2522f4b88
Yoshiki-Itagaki <b1700336@gl.aiu.ac.jp>
2024-02-13 16:08:54 +0900

    handle an input value in order to reflect changes on the screen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,35 +4,44 @@ function App() {
 
     const [headingText, setHeadingText] = useState("Hello");
     const [isMousedOver, setMouseOver] = useState(false);
+    const [name, setName] = useState("");
 
     function handleClick(){
-        setHeadingText("Submitted");
+        setHeadingText(`${headingText} ${name} `);
     }
-
+    
     function handleMouseOver(){
         setMouseOver(true);
     }
-
+    
     function handleMouseOut(){
         setMouseOver(false);
     }
 
+    function handleChange(event){
+        console.log(event.target.value);
+        setName(event.target.value)
+        setHeadingText("Hello")
+    }
+   
     return (
         <div className="container">
         <h1>{headingText}</h1>
-        <input type="text" placeholder="What's your name?" />
+        <input 
+            type="text" 
+            placeholder="What's your name?" 
+            value = {name}
+            onChange={handleChange}
+        />
         <button 
-        onClick={handleClick} 
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-        style = {{ backgroundColor : isMousedOver ? "black" : "white"}}
-        >
+            onClick={handleClick} 
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+            style = {{ backgroundColor : isMousedOver ? "black" : "white"}}>
             Submit
         </button>
         </div>
     );
 }
-
-
 
 export default App;
